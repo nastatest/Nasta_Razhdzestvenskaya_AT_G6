f6429f82e4904367430616c13710d22167fa1216
Razhdzestvenskaya <test.nr.ita@gmail.com>
2022-10-12 01:19:26 +0300

    started to do bubbles

--- src/main/java/project/boxing/Bottle.java
+++ src/main/java/project/boxing/Bottle.java
@@ -1,4 +1,29 @@
 package main.java.project.boxing;
 
 public class Bottle {
+    //есть вода
+    //есть метод open(), который вызывает метод degas() в газировке
+
+    private double volume;
+    private Water water;
+
+    public Bottle(double volume) { // бутылка заполняется массивом из пузырьков
+                                    // из рассчета 10000 на каждый литр
+    }
+
+    public void open() { // меняет состояние воды в "открытое"
+                        // (приблизительно, как this.water.setOpened(true);)
+    }
+
+    public void warm(int temperature) { //устанавливает температуру воды в бутылке
+
+    }
+
+    public Water getWater() { //возвращающий обьект воды
+
+    }
+
+    public setWater(Water water) { //добавляющий новый обьект воды
+
+    }
 }
--- src/main/java/project/water/SparklingWater.java
+++ src/main/java/project/water/SparklingWater.java
@@ -3,4 +3,37 @@ package main.java.project.water;
 import main.java.project.water.Water;
 
 public class SparklingWater  extends Water {
+    //у газировки есть пузырьки
+    //вода заполняется пузырьками при упаковке на заводе,
+    // для этого у нее есть метод pump(Bubble[] bubbles)
+    //1 литр воды содержит 10 тыс пузырьков
+    //у газировки есть метод degas(),
+    // который удаляет пузырьки по одному и вызывает их лопанье
+
+    private boolean isOpened;
+    private Bubble[] bubbles;
+
+    public SparklingWater() { // вызывает внутренний метод isOpened();
+
+    }
+
+    public void pump(Bubble[] bubbles) { // сетает массив из пузырьков в воду
+
+    }
+
+    public void setOpened(boolean isOpened) { // меняет состояние воды на "открытое"
+
+    }
+
+    private void isOpened() { // в новом потоке проверят состояние воды на "открытость"
+                                // и в случае, если она открыта запускает метод degas()
+    }
+
+    private void degas() { // каждую секунду выпускает по партии пузырьков
+                            // из рассчета 10 + 5 * температура_воды
+    }
+
+    public boolean isSparkle() { //возвращающий true если в воде еще есть пузырьки газа
+
+    }
 }
