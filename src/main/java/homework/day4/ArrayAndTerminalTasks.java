e4a4b3fd1ddaebbd545cead9ebb2cfc608ea4c1e
Razhdzestvenskaya <test.nr.ita@gmail.com>
2022-10-14 02:47:47 +0300

    changes in ArrayAndTerminalTasks

--- src/main/java/homework/day4/ArrayAndTerminalTasks.java
+++ src/main/java/homework/day4/ArrayAndTerminalTasks.java
@@ -2,11 +2,34 @@ package main.java.homework.day4;
 
 public class ArrayAndTerminalTasks {
 
+    public void print(int[] array) {
+        for (int arrayMember : array) {
+            System.out.print(arrayMember + " ");
+        }
+        System.out.println();
+    }
+
     public void sumOfElements(int[] array, int n) {
         int sum = 0;
         for (int i = n - 1; i <= array.length - 1; i += n) {
             sum += array[i];
         }
         System.out.println(sum);
+    }
+
+    public int[] inverseArray(int[] array, int n) {
+        int[] newArray = new int[] {};
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (array[i] > n) {
+                for (int newArrayIndex = 0; ; newArrayIndex++) {
+                    newArray[newArrayIndex] = array[i];
+                }
+            }
+        }
+        return newArray;
+        for (int arrayMember : newArray) {
+            System.out.print(arrayMember + " ");
+        }
+        System.out.println();
     }
 }
--- src/main/java/homework/day4/ArrayAndTerminalTasksRunner.java
+++ src/main/java/homework/day4/ArrayAndTerminalTasksRunner.java
@@ -4,7 +4,10 @@ public class ArrayAndTerminalTasksRunner {
 
     public static void main(String[] args) {
 
-        ArrayAndTerminalTasks taskOne = new ArrayAndTerminalTasks();
-        taskOne.sumOfElements(new int[]{1, 2, 3, 4, 5}, 2);
+        ArrayAndTerminalTasks task = new ArrayAndTerminalTasks();
+        task.sumOfElements(new int[]{1, 2, 3, 4, 5}, 2);
+
+        task.inverseArray(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 5);
+        task.print(newArray);
     }
 }
