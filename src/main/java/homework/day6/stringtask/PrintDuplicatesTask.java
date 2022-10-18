b994306d614214d1a33b289cef8de22053cc9b38
Razhdzestvenskaya <test.nr.ita@gmail.com>
2022-10-19 01:38:11 +0300

    PrintDuplicatesTask done

--- src/main/java/homework/day6/stringtask/PrintDuplicatesTask.java
+++ src/main/java/homework/day6/stringtask/PrintDuplicatesTask.java
@@ -1,7 +1,17 @@
 package main.java.homework.day6.stringtask;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PrintDuplicatesTask {
+    private String regex="(\\b\\w+\\b).*\\1";
+
     public void printDuplicates(String string) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(string);
+        while (matcher.find()) {
+            System.out.println(matcher.group(1));
+        }
 
     }
 }
--- src/main/java/homework/day6/stringtask/StringObjectRunner.java
+++ src/main/java/homework/day6/stringtask/StringObjectRunner.java
@@ -3,7 +3,11 @@ package main.java.homework.day6.stringtask;
 public class StringObjectRunner {
     public static void main(String[] args) {
         PrintLettersInColumnTask printLettersInColumnTask = new PrintLettersInColumnTask();
-        printLettersInColumnTask.printLettersInColumn("hello, world");
+        printLettersInColumnTask.printLettersInColumn("hello");
+        System.out.println();
+
+        PrintDuplicatesTask printDuplicatesTask = new PrintDuplicatesTask();
+        printDuplicatesTask.printDuplicates("A feature of quantifiers is the possibility of using them in different modes: greedy, super-greedy and lazy.");
         System.out.println();
     }
 }
