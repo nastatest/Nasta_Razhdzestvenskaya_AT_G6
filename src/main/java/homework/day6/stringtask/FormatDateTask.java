d4fd2a377938456d9abbd4d3f446fb5f2c4b12b5
Razhdzestvenskaya <test.nr.ita@gmail.com>
2022-10-22 04:09:38 +0300

    FormatDateTask done

--- src/main/java/homework/day6/stringtask/FormatDateTask.java
+++ src/main/java/homework/day6/stringtask/FormatDateTask.java
@@ -1,7 +1,14 @@
 package main.java.homework.day6.stringtask;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class FormatDateTask {
     public void formatDate(String dateString) {
-
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH.mm dd.MM.yyyy");
+        LocalDateTime date = LocalDateTime.parse(dateString, formatter);
+        System.out.printf("%1$tB, %1$te, %1$tY %1$tT", date);
     }
 }
