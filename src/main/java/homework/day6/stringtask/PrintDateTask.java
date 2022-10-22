72ddda645642e1a86ccb10062590851edb906c04
Razhdzestvenskaya <test.nr.ita@gmail.com>
2022-10-22 03:50:58 +0300

    PrintDateTask done

--- src/main/java/homework/day6/stringtask/PrintDateTask.java
+++ src/main/java/homework/day6/stringtask/PrintDateTask.java
@@ -1,7 +1,13 @@
 package main.java.homework.day6.stringtask;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Formatter;
+import java.util.Locale;
+
 public class PrintDateTask {
     public void printDate() {
-
+        Date date = new Date();
+        System.out.printf("Сейчас на дворе: %1$td %1$tB, %1$tY, %1$tH часа %1$tM минут\n", date);
     }
 }
--- src/main/java/homework/day6/stringtask/StringObjectRunner.java
+++ src/main/java/homework/day6/stringtask/StringObjectRunner.java
@@ -25,5 +25,12 @@ public class StringObjectRunner {
                 "access_log.2020.09.07 212.168.101.6 denied\n" +
                 "access_log.2020.09.07 21.168.101.6 granted\n");
         System.out.println();
+
+        PrintDateTask printDateTask = new PrintDateTask();
+        printDateTask.printDate();
+        System.out.println();
+
+        FormatDateTask formatDateTask = new FormatDateTask();
+        formatDateTask.formatDate("22.00 07.09.2020");
     }
 }
