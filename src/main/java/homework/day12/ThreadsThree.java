ca0fc6290f9a8ae02af3e26244a6fbe8fc50abd0
Razhdzestvenskaya <test.nr.ita@gmail.com>
2022-11-09 23:07:43 +0300

    Threads 1.3 done

---
+++ src/main/java/homework/day12/ThreadsThree.java
@@ -0,0 +1,86 @@
+package main.java.homework.day12;
+
+import java.util.ArrayList;
+
+public class ThreadsThree {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread t1 = new Thread(() -> {
+            try {
+                createAndPeepOdd();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            try {
+                createAndPeepOdd();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Thread t3 = new Thread(() -> {
+            try {
+                createAndPeepOdd();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Thread t4 = new Thread(() -> {
+            try {
+                createAndPeepOdd();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Thread t5 = new Thread(() -> {
+            try {
+                createAndPeepOdd();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Thread t6 = new Thread(() -> {
+            try {
+                createAndPeepOdd();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+        t6.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+        t5.join();
+        t6.join();
+
+    }
+
+    public static void createAndPeepOdd() throws InterruptedException {
+
+        ArrayList<Mouse> mouseCollection = new ArrayList<>();
+
+        for (int i = 1; i <= 26; i++) {
+            Mouse mouse = new Mouse(i);
+            mouseCollection.add(mouse);
+        }
+
+        for (int i = 0; i < mouseCollection.size(); i += 2) {
+            mouseCollection.get(i).peep();
+        }
+    }
+}
